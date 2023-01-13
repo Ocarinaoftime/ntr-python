9d766cb3f9a5fdd55514dd83c9bc86d6dd2188df
Ocarinaoftime <botwguardianbotw@gmail.com>
2023-01-13 09:09:20 -0500

    testing

--- oy.py
+++ oy.py
@@ -7,11 +7,16 @@ import sys
 import os
 from PyNTR.PyNTR import PyNTR
 
-ip = "10.7.41.10"
+
 
 def main():
 
-    client = PyNTR(ip)
+    parser = argparse.ArgumentParser(description='Input display for NTR connected 3DS')
+    parser.add_argument('ip', metavar='IP', help='Local IP of your 3DS')
+    args = parser.parse_args()
+    print(args)
+
+    client = PyNTR(args.ip)
     client.start_connection()
     client.send_hello_packet()
     pid = client.set_game_name('sound')
