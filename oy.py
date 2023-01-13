95f5030a5e178336c934c2f9c967d7a0df3419bd
Ocarinaoftime <botwguardianbotw@gmail.com>
2023-01-13 15:59:01 -0500

    upload for pc

--- oy.py
+++ oy.py
@@ -1,13 +1,31 @@
 import argparse
 import struct
-import time
 import pygame
 import distutils.util
-import sys
-import os
 from PyNTR.PyNTR import PyNTR
 
+class SoundState:
 
+	def __init__(self, client):
+		self.client = client
+
+	def update(self):
+		base = 0x1EC03000
+
+		combined_state = self.client.ReadU64(base + 0x40C)
+		[
+            sound_state
+		] = (struct.unpack('hhhh', combined_state.tobytes(8, 'little')))
+
+		[
+			self.volume_level,
+			_,_,_,_,
+		] = map(distutils.util.strtobool, list(format(sound_state, '016b'))[::-1])
+
+	def p(self):
+		print('=======================')
+		print('button_a',     self.volume_level)
+		print('=======================')
 
 def main():
 
@@ -16,19 +34,35 @@ def main():
     args = parser.parse_args()
     print(args)
 
+    pygame.init()
+
     client = PyNTR(args.ip)
     client.start_connection()
     client.send_hello_packet()
-    pid = client.set_game_name('sound')
+    pid = client.set_game_name('hid')
+
+    sound_state = SoundState(client)
 
     addr = 0x1EC0340C
     leng = addr + 0x4
 
     closed = False
 
+    clock = pygame.time.Clock()
+
     while not closed:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 closed = True
-        
-        client.send_read_memory_packet(addr, leng)
+
+        sound_state.update()
+        sound_state.p()
+
+        client.send_read_memory_packet(addr, 8)
+        clock.tick(60)
+    
+
+
+
+
+if __name__ == "__main__": main()
